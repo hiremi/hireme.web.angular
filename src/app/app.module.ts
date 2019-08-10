1d244b51178172856b48a598250aeed4b5ad0082
Antonio Johnson <antoniookeefejohnson@gmail.com>
2019-08-10 08:46:53 -0500

    Updated website with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
